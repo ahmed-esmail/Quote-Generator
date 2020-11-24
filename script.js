dbe74f4ed81d2251a50523f5e90aa3ef3de864c3
ahmed-esmail <51207001+ahmed-esmail@users.noreply.github.com>
2020-11-24 22:13:41 +0200

    improve ui

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-
-const quoteContainer = document.getElementById('quote-container');
-const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
 //Get quote From API
 async function getQuote() {
-   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-   try {
-       const response = await fetch(proxyUrl + apiUrl);
-       const data = await response.json();
-       authorText.innerText = data.quoteAuthor;
-       quoteText.innerText = data.quoteText;
-       console.log(data);
-   } catch (error) {
-       console.log('whoops, no quote', error);
-   }
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    //if author is blink 
+    if (data.authorText === "") {
+      authorText.innerText = "unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
+    console.log(data);
+  } catch (error) {
+    console.log("whoops, no quote", error);
+  }
 }
+
+
 
 //on Load
 getQuote();
